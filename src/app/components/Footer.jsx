571bd5e69f458a2e916a5c311b32bcf31b381081
unknown <gokull.mani@outlook.com>
2024-01-30 19:53:12 -0600

    Portfolio Commit 3

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p className="text-slate-600 lg:mr-2">
+          ©Gokul Mani. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,12 +2,19 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-
+// import Resume from "../../../public/pdf/GokulManiResume.pdf";
+import { motion } from "framer-motion";
+import Link from "next/link";
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 3 }}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello I'm {""}
@@ -33,17 +40,35 @@ const HeroSection = () => {
             Random text
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire Me
-            </button>
+            <Link
+              href="https://www.linkedin.com/in/gokul-mani/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                Linkedin
+              </button>
+            </Link>
+            {/* <a
+              href={require("../../../public/pdf/GokulMani_Resume.pdf")}
+              download="myFile"
+            > */}
+            {/* <Link href={Resume} target="_blank" rel="noopener noreferrer"> */}
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download Resume
               </span>
             </button>
+            {/* </Link> */}
+            {/* </a> */}
           </div>
-        </div>
-        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 3 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
@@ -53,7 +78,7 @@ const HeroSection = () => {
               height={300}
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
